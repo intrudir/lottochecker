7f7053721286ed535c68d8cc45664da1d5b14351
Intrudir <24526564+intrudir@users.noreply.github.com>
2020-12-01 20:07:22 -0500

    Update lottochecker.py

--- lottochecker.py
+++ lottochecker.py
@@ -1,101 +1,46 @@
-import argparse
+import argparse, yaml, sys
 from functions import *
-from datetime import date
+
+# creds to send email with results
+user = "some@gmail.com"
+password = ""
 
 parser = argparse.ArgumentParser(
 description="This script is used to check for lotto numbers and see if you won."
 )
-parser.add_argument('--powerball', action="store_true", default=False, dest='powerball',
-	help="check Powerball numbers")
-parser.add_argument('--megamillions', action="store_true", default=False, dest='megamillions',
-	help="check Megamillions numbers")
-parser.add_argument('--cash4life', action="store_true", default=False, dest='cash4life',
-	help="check Cash 4 Life numbers")
-parser.add_argument('--lotto', action="store_true", default=False, dest='lotto',
-	help="check Lotto numbers")
+parser.add_argument(
+    '-c', '--check',
+    action="store", default=False, dest='check',
+    choices=("powerball", "megamillions", "lotto"),
+    help="check winning numbers for specified game")
 
 args = parser.parse_args()
 
-today = date.today()
-ballName = ['Powerball', 'Megaball', 'Cashball', 'Lotto']
-url = [
-    "http://floridalottery.com/site/powerball",
-    "http://floridalottery.com/site/megaMillions",
-    "http://floridalottery.com/cash4Life",
-    "http://floridalottery.com/site/lotto"
-]
-bigBallPath = [
-    '//span[@class="balls pbBall"]/text()',
-    '//span[@class="balls mMillBall"]/text()',
-    '//span[@class="balls c4lCBBall"]/text()'
-]
-myPicks = {
-    'powerball': {'balls': ['6', '8', '10', '28', '49'], 'bigBall': '7'},
-    'megaMills': {'balls': ['6', '8', '10', '28', '49'], 'bigBall': '7'},
-    'megaMills2': {'balls': ['7', '12', '21', '36', '51'], 'bigBall': '20'},
-    'cash4Life': {'balls': ['6', '8', '10', '28', '49'], 'bigBall': '4'},
-    'lotto': {'balls': ['6', '8', '10', '14', '28', '49']},
-    'Mom\'s powerball': {'balls': ['10', '17', '25', '26', '40'], 'bigBall': '7'},
-    'Mom\'s powerball2': {'balls': ['3', '17', '21', '58', '63'], 'bigBall': '5'},
-    'Mom\'s powerball3': {'balls': ['7', '12', '21', '36', '51'], 'bigBall': '20'},
-    'Mom\'s megaMills': {'balls': ['3', '17', '21', '58', '63'], 'bigBall': '5'},
-    'Mom\'s lotto': {'balls': ['10', '13', '25', '26', '30', '40']},
-    'Mom\'s lotto2': {'balls': ['7', '31', '37', '40', '42', '50']}
-}
-
-if args.powerball:
-    mailSubject = "Powerball"
-    balls, bigBall, winningNumsMsg = getWinningNums(args, url[0], bigBallPath[0], ballName[0])
-    finalResults = winningNumsMsg
-    for game,picks in myPicks.items():
-        if 'powerball' in game:
-            resultsMsg = checkWin(args, balls, bigBall, ballName[0], picks)
-            finalResults += resultsMsg
-
-elif args.megamillions:
-    mailSubject = "Megamillions"
-    balls, bigBall, winningNumsMsg = getWinningNums(args, url[1], bigBallPath[1], ballName[1])
-    finalResults = winningNumsMsg
-    for game,picks in myPicks.items():
-        if 'megaMills' in game:
-            resultsMsg = checkWin(args, balls, bigBall, ballName[1], picks)
-            finalResults += resultsMsg
-
-elif args.cash4life:
-    mailSubject = "Cash 4 Life"
-    balls, bigBall, winningNumsMsg = getWinningNums(args, url[2], bigBallPath[2], ballName[2])
-    finalResults = winningNumsMsg
-    for game,picks in myPicks.items():
-        if 'cash4Life' in game:
-            resultsMsg = checkWin(args, balls, bigBall, ballName[2], picks)
-            finalResults += resultsMsg
-
-elif args.lotto:
-    mailSubject = "Lotto"
-    balls, bigBall, winningNumsMsg = getWinningNums(args, url[3], bigBallPath, ballName[3])
-    finalResults = winningNumsMsg
-    for game,picks in myPicks.items():
-        if 'lotto' in game:
-            resultsMsg = checkWin(args, balls, bigBall, ballName[3], picks)
-            finalResults += resultsMsg
-
-else:
+# if no args print help() and exit
+if len(sys.argv) <= 1:
     parser.print_help()
     print()
-    exit()
+    sys.exit(0)
 
-print (finalResults)
+# Get numbers from numbers.yaml
+try:
+    with open("numbers.yaml") as s:
+        numbers = yaml.load(s, Loader=yaml.FullLoader)
+except FileNotFoundError:
+    print("You need to add your picks to 'numbers.yaml'. A template has been \
+created for you.")
+    createNumbers()
+    sys.exit(1)
+
+# Get winning numbers
+highest = 0
+winners = getWinningNumbers(args.check)
+msg, matched, highest = checkMyNums(args.check, numbers, winners, highest)
+print(msg)
 
 # send the results by email
+mailSubject = "{}: highest numbers matched: {}".format(args.check, highest)
 try:
-    from secrets import creds
-except:
-    print("You need to configure your email credentials. \
-    \nAn example 'secrets.py' file has been created. Please add your email creds.")
-    createSecrets()
-    exit()
-try:
-    sendMail(creds, mailSubject, finalResults)
+    sendMail(user, password, mailSubject, msg)
 except smtplib.SMTPAuthenticationError:
-    print("Invalid email credentials. \
-    \nPlease revise secrets.py")
+    print("Invalid email credentials.")
