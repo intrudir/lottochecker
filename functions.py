c75e610bf5fc0055d0a3e5d53ac544ab2fe5f368
Intrudir <24526564+intrudir@users.noreply.github.com>
2020-12-01 20:07:54 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,115 +1,93 @@
 import requests, smtplib
-from lxml import html
+from datetime import date
+from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:68.0) Gecko/20100101 Firefox/68.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-}
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
 
-def createSecrets():
-    content = """# Change email and password values
-creds = {
-'user': "xxxxx@gmail.com",
-'passwd': "xxxxxx"
-}
+def createNumbers():
+    content = """\
+my numbers:
+  powerball:
+    - 1
+    - 2
+    - 3
+    - 4
+    - 5
+    - 6
+
+  powerball2:
+    - 1
+    - 2
+    - 3
+    - 4
+    - 5
+    - 6
+
+moms numbers:
+  megamillions:
+    - 1
+    - 2
+    - 3
+    - 4
+    - 5
+    - 6
 """
-    with open('secrets.py', 'w') as fp:
-        fp.write(content)
+    with open('numbers.yaml', 'w') as nf:
+        nf.write(content)
     return True
 
 
-def getWinningNums(args, url, bigBallPath, ballName):
-    page = requests.get(url, headers=headers)
-    tree = html.fromstring(page.content)
-    balls = tree.xpath('//span[@class="balls"]/text()')
-    jackpotDate = tree.xpath('//div[@class="gamePageNumbers"]/p/text()')[1]
+def getWinningNumbers(check):
+    # For some reason megaMillions has to be spelled this way
+    if check == "megamillions":
+        check = "megaMillions"
 
-    if not args.lotto:
-        balls = balls[:5]
-        bigBall = tree.xpath(bigBallPath)
-        bigBall = bigBall[0]
-        bbMsg = "Today's {}: {}".format(ballName, bigBall)
-    else:
-        balls = balls[:6]
-        bigBall = ''
-        bbMsg = ''
+    url = "https://www.flalottery.com:443/{}".format(check)
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    gamePageBalls = soup.find("div", {"class": "gamePageBalls"})
+    balls = gamePageBalls.find_all("span", {"class": "balls"})
+    winnerBalls = []
+    for b in balls:
+        try:
+            winnerBalls.append(int(b.text))
+        except ValueError:
+            pass
 
-    winningNumsMsg = (
-"""
-{0}
-Jackpot Date: {1}
-{2}
-Today's numbers: {3}
-{0}
-""".format('*' * 40, jackpotDate, bbMsg, ','.join(balls)))
-
-    return balls, bigBall, winningNumsMsg
+    return winnerBalls
 
 
-def checkWin(args, balls, bigBall, ballName, picks):
-    matched = []
-    bbMsg = ''
-    if not args.lotto:
-        bbMsg = "Your {}: {}".format(ballName, picks['bigBall'])
-        for ball in picks['balls']:
-            if ball in balls[:5]:
-                matched.append(ball)
+def checkMyNums(check, numbers, winningNums, highest):
+    d = date.today()
+    msg = "Date: {}\n".format(d)
+    msg += "Winning {} numbers: {}\n".format(check, winningNums)
 
-        if picks['bigBall'] == bigBall:
-            matchMsg = "{} matched!".format(ballName)
-            matchBB = True
-        else:
-            matchMsg = "{} did not match.".format(ballName)
-            matchBB = False
-    else:
-        for ball in picks['balls']:
-            if ball in balls[:6]:
-                matched.append(ball)
+    # my numbers:
+    for picks in numbers:
+        msg += "\n{}: ".format(picks)
 
-    if args.powerball or args.megamillions:
-        if len(matched) == 5 and matchBB == True:
-            result = "CHECK THEM NOW. YOU MIGHT HAVE WON THE JACKPOT."
-        elif len(matched) >= 1 and matchBB == True:
-            result = "You matched {} numbers and the {}! You may have won something, check online".format(len(matched), ballName)
-        elif len(matched) == 0 and matchBB == True:
-            result = "You matched {} numbers but got the {}! You may have won something (free ticket or ~10$). Check online.".format(len(matched), ballName)
-        elif len(matched) >= 3 and matchBB == False:
-            result = "You matched {} numbers but no {}. You may have won something, check online.".format(len(matched), ballName)
-        else:
-            result = "Nothing yet. Keep playing!"
+        # powerball: [1, 2, 3, 4, 5, 6]
+        for game in numbers[picks]:
+            matched = 0
+            if game.lower().find(check) != -1:
+                msg += "\n    {}: {}".format(game, numbers[picks][game])
+                for n in numbers[picks][game]:
+                    if n in winningNums:
+                        matched += 1
+                msg += "\n    {} numbers matched\n".format(matched)
 
-    elif args.cash4life:
-        if len(matched) == 5 and matchBB == True:
-            result = "CHECK THEM NOW. YOU MIGHT HAVE WON THE JACKPOT."
-        elif len(matched) >= 1 and matchBB == True:
-            result = "You matched {} numbers and got the CB! You may have won something (free ticket or ~10$). Check online.".format(len(matched))
-        elif len(matched) >= 2 and matchBB == False:
-            result = "You matched {} numbers but no CB. You may have won something, check online.".format(len(matched))
-        else:
-            result = "Nothing yet. Keep playing!"
+            if matched:
+                if matched > highest:
+                    highest = matched
 
-    elif args.lotto:
-        if len(matched) == 6:
-            result = "CHECK THEM NOW. YOU MIGHT HAVE WON THE JACKPOT."
-        elif len(matched) >= 2:
-            result = "You matched {} numbers! You may have won something, Check online".format(len(matched))
-        else:
-            result = "Nothing yet. Keep playing!"
+    return msg, matched, highest
 
-    resultsMsg = (
-"""
-{}
-Picks: {}
 
-{} numbers matched: {}
-{}
-\n{}
-""".format(bbMsg, ','.join(picks['balls']), len(matched), ','.join(matched), result, '*' * 40))
-
-    return resultsMsg
-
-def sendMail(creds, subject, finalResults):
+def sendMail(user, password, subject, finalResults):
     to_email = "lottoscript1@gmail.com"
     from_email = "lottoscript1@gmail.com"
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -120,5 +98,5 @@ def sendMail(creds, subject, finalResults):
     msg['To'] = to_email
     msg['From'] = from_email
     msg['Subject'] = subject
-    server.login(creds['user'], creds['passwd'])
+    server.login(user, password)
     server.send_message(msg)
