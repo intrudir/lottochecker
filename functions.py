4898ab445e15747520df54dcd7eee48cd18f2b31
Intrudir <24526564+intrudir@users.noreply.github.com>
2020-12-08 10:12:16 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -42,16 +42,24 @@ moms numbers:
     return True
 
 
-def getWinningNumbers(check):
-    # For some reason megaMillions has to be spelled this way
-    if check == "megamillions":
-        check = "megaMillions"
+def get_game_data(game):
+    """ Fetch game data like:
+    jackpot numbers,
+    next jackpot date & amount,
+    winnings table that describes what you get when you win X of X numbers.
+    """
 
-    url = "https://www.flalottery.com:443/{}".format(check)
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    gamePageBalls = soup.find("div", {"class": "gamePageBalls"})
-    balls = gamePageBalls.find_all("span", {"class": "balls"})
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+
+    url = "https://www.flalottery.com:443/{}".format(game)
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.content, 'html.parser')
+
+    # get winning jackpot numbers
+    game_page_balls = soup.find_all("div", {"class": "gamePageBalls"})
 
     # get next jackpot date and amount
     jackpotDiv = soup.find("div", {"class": "nextJackpot"}).find_all("p")
@@ -62,71 +70,84 @@ def getWinningNumbers(check):
     # get winnings table
     winTable = soup.find("table", {"class": "style1 games"})
 
-    winnerBalls = []
-    for b in balls:
+    return game_page_balls, jackpot, winTable
+
+
+def get_jackpot_nums(game_page_balls, index):
+    balls = game_page_balls[index].find_all("span", {"class": "balls"})
+
+    winners = []
+    for ball in balls:
         try:
-            winnerBalls.append(int(b.text))
+            winners.append(int(ball.text))
         except ValueError:
             pass
 
-    return winnerBalls, jackpot, winTable
+    return winners
 
 
-def checkMyNums(check, myNumbers, winningNums, jackpot, highest):
-    d = date.today()
-    msg = "Date: {}\n".format(d)
-    msg += "Winning {} numbers: {}\n\n".format(check, winningNums)
-    for k, v in jackpot.items():
-        msg += "{}".format(k)
-        msg += "{}\n".format(v)
-
+def check_win(game_name, my_numbers, winners):
+    msg_body = ""
+    highest = 0
     # my numbers:
-    for picks in myNumbers:
-        msg += "\n{}: ".format(picks)
+    for picks in my_numbers:
+        msg_body += "\n{}: ".format(picks)
 
         # powerball: [1, 2, 3, 4, 5, 6]
-        for game in myNumbers[picks]:
+        for game in my_numbers[picks]:
             matched = 0
             pbMatched = False
-            if game.lower().find(check) != -1:
-                msg += "\n    {}: {}".format(game, myNumbers[picks][game])
+            if game.lower().find(game_name) != -1:
+                msg_body += "\n    {}: {}\n".format(game, my_numbers[picks][game])
 
-                if check == "powerball" or check == "megamillions":
-                    for n in myNumbers[picks][game][:5]:
-                        if n in winningNums[:5]:
+                if game_name == "lotto":
+                    for n in my_numbers[picks][game]:
+                            if n in winners:
+                                matched += 1
+                    msg_body += "    {} numbers matched\n".format(matched)
+
+                if game_name == "powerball" or game_name == "megamillions":
+                    for n in my_numbers[picks][game][:5]:
+                        if n in winners[:5]:
                             matched += 1
+                    msg_body += "    {} numbers matched\n".format(matched)
 
-                    if myNumbers[picks][game][-1] == winningNums[-1]:
+                    if my_numbers[picks][game][-1] == winners[-1]:
                         pbMatched = True
 
-                    msg += "\n    ball matched: {}".format(pbMatched)
-
-                if check == "lotto":
-                    for n in myNumbers[picks][game]:
-                        if n in winningNums:
-                            matched += 1
-
-                msg += "\n    {} numbers matched\n".format(matched)
+                    msg_body += "    ball matched: {}\n".format(pbMatched)
 
             if matched:
                 if matched > highest:
                     highest = matched
 
-    return msg, matched, highest
+    return msg_body, highest
 
 
-def getHistory(check):
-    if check == "powerball":
-        check = "pb.htm"
-    elif check == "megamillions":
-        check = "mmil.htm"
-    elif check == "lotto":
-        check = "l6.htm"
+def prep_msg(game_name, game_type, winners, jackpot_info, msg_body):
+    d = date.today()
+    if game_type == "double play":
+        jackpot_info["Jackpot amount: "] = "Check double play table"
 
+    msg_head = """\
+Game: {} {}
+Date: {}
+Winning numbers: {}
+
+Next draw date: {}
+Jackpot amount: {}
+""".format(game_type, game_name, d, winners, jackpot_info["Next Jackpot: "], jackpot_info["Jackpot amount: "])
+
+    msg = msg_head + msg_body
+
+    return msg
+
+
+def get_history(page):
+    url = "https://www.flalottery.com/exptkt/{}".format(page)
     drawDate = ""
-    historyWinners = {}
+    history_winners = {}
     numList = []
-    url = "https://www.flalottery.com/exptkt/{}".format(check)
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     tables = soup.find_all("table")
@@ -156,50 +177,34 @@ def getHistory(check):
                 # add numbers to a dictionary with draw date
                 # then reset number list
                 if len(numList) == 6:
-                    if drawDate in historyWinners:
-                        historyWinners[drawDate + " DP"] = numList
+                    if drawDate in history_winners:
+                        history_winners[drawDate + " DP"] = numList
                         numList = []
                         drawDate = ""
                     else:
-                        historyWinners[drawDate] = numList
+                        history_winners[drawDate] = numList
                         numList = []
                         drawDate = ""
-    return historyWinners
+
+    return history_winners
 
 
-def checkHistory(check, myNumbers, historyWinners):
-    msg = ""
+def check_history(game_name, my_numbers, history_winners):
     # my numbers:
-    for picks in myNumbers:
+    for picks in my_numbers:
         print("\n{}: ".format(picks))
 
-        for game in myNumbers[picks]:
-            if game.lower().find(check) != -1:
+        for game in my_numbers[picks]:
+            if game.lower().find(game_name) != -1:
                 # powerball: [1, 2, 3, 4, 5, 6]
-                print("    {}: {}\n".format(game, myNumbers[picks][game]))
+                print("    {}: {}\n".format(game, my_numbers[picks][game]))
 
-                for drawDate, hNumbers in historyWinners.items():
+                for drawDate, hNumbers in history_winners.items():
                     matched = 0
                     pbMatched = False
 
-                    if check == "powerball" or check == "megamillions":
-                        for n in myNumbers[picks][game][:5]:
-                            if str(n) in hNumbers[:5]:
-                                matched += 1
-                        if str(myNumbers[picks][game][-1]) == hNumbers[-1]:
-                            pbMatched = True
-
-                        if matched == 5 and pbMatched is True:
-                            print("    ALL 6 NUMBERS MATCHED: {}: {}".format(drawDate, hNumbers))
-                            print()
-
-                        if matched >= 3:
-                            print("    3 or more numbers matched: {}: {}".format(drawDate, hNumbers))
-                            print("    {} ball matched?: {}".format(check, pbMatched))
-                            print()
-
-                    if check == "lotto":
-                        for n in myNumbers[picks][game]:
+                    if game_name == "lotto":
+                        for n in my_numbers[picks][game]:
                             if str(n) in hNumbers:
                                 matched += 1
 
@@ -210,10 +215,76 @@ def checkHistory(check, myNumbers, historyWinners):
                         if matched >= 4:
                             print("4 or more numbers matched: {}: {}".format(drawDate, hNumbers))
                             print()
+
+                    if game_name == "powerball" or game_name == "megamillions":
+                        for n in my_numbers[picks][game][:5]:
+                            if str(n) in hNumbers[:5]:
+                                matched += 1
+                        if str(my_numbers[picks][game][-1]) == hNumbers[-1]:
+                            pbMatched = True
+
+                        if matched == 5 and pbMatched is True:
+                            print("    ALL 6 NUMBERS MATCHED: {}: {}".format(drawDate, hNumbers))
+                            print()
+
+                        if matched >= 3:
+                            print("    3 or more numbers matched: {}: {}".format(drawDate, hNumbers))
+                            print("    {} ball matched?: {}".format(game_name, pbMatched))
+                            print()
+
     exit()
 
 
-def sendMail(user, password, subject, finalResults):
+class Game:
+    """ Do stuff for a particular lottery game."""
+
+    def __init__(self, game, game_type):
+        self.game = game
+        self.game_type = game_type
+
+    def get_win_data(self):
+        """Get winning numbers."""
+
+        game_page_balls, jackpot_info, win_table = get_game_data(self.game)
+        self.game = self.game.lower()
+
+        if self.game_type == "double play":
+            winners = get_jackpot_nums(game_page_balls, 1)
+        else:
+            winners = get_jackpot_nums(game_page_balls, 0)
+
+        return winners, jackpot_info, win_table
+
+    def check_win(self, my_numbers, winners):
+        """Check your provided numbers against winning nums."""
+
+        msg_body, highest = check_win(self.game, my_numbers, winners)
+
+        return msg_body, highest
+
+    def prep_msg(self, winners, jackpot_info, msg_body):
+        """Prepare final results message for print and email."""
+
+        msg = prep_msg(self.game, self.game_type, winners, jackpot_info, msg_body)
+
+        return msg
+
+    def check_historical_data(self, my_numbers):
+        """check against historical win data for a specified game."""
+
+        if self.game == "powerball":
+            history_winners = get_history("pb.htm")
+
+        elif self.game == "megamillions":
+            history_winners = get_history("mmil.htm")
+
+        elif self.game == "lotto":
+            history_winners = get_history("l6.htm")
+
+        check_history(self.game, my_numbers, history_winners)
+
+
+def send_mail(user, password, subject, finalResults):
     to_email = "lottoscript1@gmail.com"
     from_email = "lottoscript1@gmail.com"
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -226,3 +297,4 @@ def sendMail(user, password, subject, finalResults):
     msg['Subject'] = subject
     server.login(user, password)
     server.send_message(msg)
+    
