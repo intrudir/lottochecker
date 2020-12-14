58e7b3192075e20b6b766159c9faeadd7af071cd
Intrudir <24526564+intrudir@users.noreply.github.com>
2020-12-14 09:32:41 -0500

    Update lottochecker.py

--- lottochecker.py
+++ lottochecker.py
@@ -15,6 +15,11 @@ parser.add_argument(
     help="check winning numbers for specified game")
 
 parser.add_argument(
+    '-s', '--single',
+    action="store", default=None, dest='single',
+    help="Check numbers you specify")
+
+parser.add_argument(
     '-H', '--history',
     action="store_true", default=False, dest='history',
     help="check your numbers against historical data for a game")
@@ -38,56 +43,79 @@ created for you.")
     createNumbers(scriptDir)
     sys.exit(1)
 
+if args.single:
+    nums = args.single.split(',')
+    if len(nums) != 6:
+        print("You need to specify 6 numbers. \
+If checking MM or PB, that ball should be #6.")
+        sys.exit(1)
+    else:
+        # need to turn nums into ints
+        try:
+            to_int = map(int, nums)
+            my_numbers = {"single": {args.check: list(to_int)}}
+        except ValueError:
+            print("Something wrong with one of your specified numbers.")
+            sys.exit(1)
 
 if args.check == "lotto":
+    Lotto = Game("lotto", "regular")
     if args.history:
-        Lotto = Game("lotto", None)
         Lotto.check_historical_data(my_numbers)
     else:
-        Lotto = Game("lotto", "regular")
         winners, jackpot_info, win_table = Lotto.get_win_data()
-        msg_body, highest = Lotto.check_win(my_numbers, winners)
+        msg_body, highest, highest_PB = Lotto.check_win(my_numbers, winners)
         msg = Lotto.prep_msg(winners, jackpot_info, msg_body)
         print(msg)
 
 elif args.check == "lotto DP":
+    Lotto_DP = Game("lotto", "double play")
     if args.history:
-        Lotto = Game("lotto", None)
         Lotto.check_historical_data(my_numbers)
     else:
-        Lotto_DP = Game("lotto", "double play")
         winners, jackpot_info, win_table = Lotto_DP.get_win_data()
-        msg_body, highest = Lotto_DP.check_win(my_numbers, winners)
+        msg_body, highest, highest_PB = Lotto_DP.check_win(my_numbers, winners)
         msg = Lotto_DP.prep_msg(winners, jackpot_info, msg_body)
         print(msg)
 
 elif args.check == "powerball":
+    Powerball = Game("powerball", "regular")
     if args.history:
-        Powerball = Game("powerball", None)
         Powerball.check_historical_data(my_numbers)
     else:
-        Powerball = Game("powerball", "regular")
         winners, jackpot_info, win_table = Powerball.get_win_data()
-        msg_body, highest = Powerball.check_win(my_numbers, winners)
+        msg_body, highest, highest_PB = Powerball.check_win(my_numbers, winners)
         msg = Powerball.prep_msg(winners, jackpot_info, msg_body)
         print(msg)
 
 elif args.check == "megamillions":
+    Megamillions = Game("megaMillions", "regular")
     if args.history:
-        Megamillions = Game("megamillions", None)
         Megamillions.check_historical_data(my_numbers)
     else:  # Needs to be spelled like megaMillions
-        Megamillions = Game("megaMillions", "regular")
         winners, jackpot_info, win_table = Megamillions.get_win_data()
-        msg_body, highest = Megamillions.check_win(my_numbers, winners)
+        msg_body, highest, highest_PB = Megamillions.check_win(my_numbers, winners)
         msg = Megamillions.prep_msg(winners, jackpot_info, msg_body)
         print(msg)
 
 # Send email with results
-mailSubject = "{}: highest numbers matched: {}".format(args.check, highest)
-try:
-    send_mail(user, password, mailSubject, msg)
-except smtplib.SMTPAuthenticationError:
-    print("Invalid email credentials.")
+if highest_PB:
+    if highest == 5:
+        mailSubject = "{}: YOU WON!!! CHECK NOW".format(args.check)
+    elif highest < 5:
+        mailSubject = "{}: highest numbers matched: {} and PB Match!".format(args.check, highest)
+elif highest == 6:
+    mailSubject = "{}: YOU WON!!! CHECK NOW".format(args.check)
+else:
+    mailSubject = "{}: highest numbers matched: {}".format(args.check, highest)
+
+if args.single:
+    print(mailSubject)
+else:
+    try:
+        send_mail(user, password, mailSubject, msg)
+        print(mailSubject)
+    except smtplib.SMTPAuthenticationError:
+        print("Invalid email credentials.")
 
 sys.exit(0)
